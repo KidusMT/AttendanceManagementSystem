5a47ebf3decd04b74968e9a32bb0cf09eaeb94af
KidusMT <kidusmamuye@gmail.com>
2021-02-27 14:31:43 -0600

    first commit

---
+++ AttendanceManagementSystem/src/main/java/com/miu/edu/cs/mpp/project/AttendanceManagementSystem/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.miu.edu.cs.mpp.project.AttendanceManagementSystem;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
