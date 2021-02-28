aa8da7b4a5d33927fac82cb7aa14dee2410f4588
brukeabebe <brukeabebe2@gmail.com>
2021-02-27 18:15:18 -0600

    1st fxml for capture attendance welcome  fxml

--- AttendanceManagementSystem/src/main/java/module-info.java
+++ AttendanceManagementSystem/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module com.miu.edu.cs.mpp.project.AttendanceManagementSystem {
     requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+
     exports com.miu.edu.cs.mpp.project.AttendanceManagementSystem;
+    exports com.miu.edu.cs.mpp.project.AttendanceManagementSystem.ui.captureattendance;
 }
