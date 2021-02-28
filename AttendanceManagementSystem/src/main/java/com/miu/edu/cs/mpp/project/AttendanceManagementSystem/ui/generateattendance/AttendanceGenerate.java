29a5d38fb1f10db2ce0c6461643e0425a9415d00
KidusMT <kidusmamuye@gmail.com>
2021-02-28 10:21:02 -0600

    updated the attendance generation page

--- AttendanceManagementSystem/src/main/java/com/miu/edu/cs/mpp/project/AttendanceManagementSystem/ui/generateattendance/AttendanceGenerate.java
+++ AttendanceManagementSystem/src/main/java/com/miu/edu/cs/mpp/project/AttendanceManagementSystem/ui/generateattendance/AttendanceGenerate.java
@@ -1,19 +1,29 @@
 package com.miu.edu.cs.mpp.project.AttendanceManagementSystem.ui.generateattendance;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 import com.miu.edu.cs.mpp.project.AttendanceManagementSystem.SystemInfo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class AttendanceGenerate extends Application {
 
+	public AttendanceGenerate() {
+		
+	}
 	
 	public void start(Stage stage) throws IOException {
         var javaVersion = SystemInfo.javaVersion();
@@ -29,4 +39,40 @@ public class AttendanceGenerate extends Application {
     public static void main(String[] args) {
         launch();
     }
+    
+    @FXML
+    public void initialize() {
+//    	fromDateField;
+    }
+    
+    @FXML
+	public DatePicker fromDateField;
+	
+	@FXML
+    public DatePicker toDateField;
+	
+	@FXML
+	public void findRecord(ActionEvent event) {
+		
+	}
+     
+     @FXML
+     public void fetchThisWeekAttendance(ActionEvent event) 
+     {
+    	 LocalDate startDate = LocalDate.now().minusWeeks(1);
+    	 LocalDate endDdate = LocalDate.now();
+    	 System.out.println(startDate.toString() +" <> "+ endDdate.toString());
+    	 fromDateField.setValue(startDate);
+    	 toDateField.setValue(endDdate);
+     };
+     
+     @FXML
+     public void fetchThisMonthAttendance(ActionEvent event) 
+     {
+    	 LocalDate startDate = LocalDate.now().minusMonths(1);
+    	 LocalDate endDdate = LocalDate.now();
+    	 System.out.println(startDate.toString() +" <> "+ endDdate.toString());
+    	 fromDateField.setValue(startDate);
+    	 toDateField.setValue(endDdate);
+     };
 }
